57015c5a12d9c8aff43d4b797cc0116e552a9ff1
Mirco <mirco.zeiss@gmail.com>
2013-03-15 20:17:03 +0100

    fix eol and async code

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ MyStream.prototype._transform = function(chunk, encoding, done) {
   var that = this;
 
   console.log('--------------');
-//  var chunk = chunk.toString();
   that._chunk = chunk.toString();
   console.log('data: ' + that._data);
   console.log('chunk: ' + that._chunk);
@@ -45,38 +44,16 @@ MyStream.prototype._transform = function(chunk, encoding, done) {
       console.log('result');
       console.log(result);
       that._data = that._data.split(result[0]).join('');
-      if (!this._headerWritten) {
-        that.writeHeader(result[0], function(err) {
-          that.writeLine(result[0], '_data', function(err) {
-
-          });
-        });
-      } else {
-        that.writeLine(result[0], '_data', function(err) {
-
-        });
-      }
+      if (!this._headerWritten) that.writeHeader(result[0]);
+      that.writeLine(result[0], '_data');
     }
   } else {
     var rex = /(\{[^}]+\})/g;
     var m = rex.exec(that._chunk);
     console.log(m);
-    if (!this._headerWritten) {
-      that.writeHeader(m[0], function(err) {
-        for (m; m; m = rex.exec(that._chunk)) {
-          that.writeLine(m[0], '_chunk');
-//      console.log(m[1]);
-//          that.emit('jsonFound', m[0])
-//          that._data = that._chunk.split(m[0]).join('');
-        }
-      });
-    } else {
-      for (m; m; m = rex.exec(that._chunk)) {
-//      console.log(m[1]);
-//        that.emit('jsonFound', m[0])
-//        that._data = chunk.split(m[0]).join('');
-        that.writeLine(m[0], '_chunk');
-      }
+    if (!this._headerWritten) that.writeHeader(m[0]);
+    for (m; m; m = rex.exec(that._chunk)) {
+      that.writeLine(m[0], '_chunk');
     }
   }
   done();
@@ -86,22 +63,20 @@ MyStream.prototype._transform = function(chunk, encoding, done) {
 /**
  * write the header line
  */
-MyStream.prototype.writeHeader = function(header, cb) {
+MyStream.prototype.writeHeader = function(header) {
+  header = JSON.parse(header);
   var that = this;
   var iterator = function(item, callback) {
     that._header.push(item);
     callback(null);
   };
-  header = JSON.parse(header);
   async.each(Object.keys(header), iterator, function(err) {
     var headerLine = that._header.join(',');
-    headerLine += os.EOL;
     console.log(that._header);
     that.emit('header', headerLine);
     that.push(headerLine);
     that._headerWritten = true;
     that._header = null;
-    cb(null);
   });
 };
 
@@ -120,7 +95,7 @@ MyStream.prototype.writeLine = function(line, bucket) {
   };
   async.each(Object.keys(lineJson), iterator, function(err) {
     var lineStr = that._line.join(',');
-    lineStr += os.EOL;
+    lineStr = os.EOL + lineStr;
     that.emit('line', lineStr);
     that.push(lineStr);
     that._data = that[bucket].split(line).join('');
