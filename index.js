117fb7b6ae36f84d07746602db35f6078d2d8e93
Mirco <mirco.zeiss@gmail.com>
2013-03-15 21:40:08 +0100

    add optional custom delimiter

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ var util = require('util');
 var async = require('async');
 var os = require('os');
 
-var MyStream = function() {
+var MyStream = function(options) {
 
   if (!(this instanceof MyStream))
     return new MyStream();
 
   Transform.call(this);
+
+  if (!options) options = {};
+
+  this.del = options.del || ',';
 
   this._headerWritten = false;
   this._header = [];
@@ -35,15 +39,17 @@ MyStream.prototype._transform = function(chunk, encoding, done) {
   var isChunkJSON = re.test(that._chunk);
   console.log('is chunk json -> ' + isChunkJSON);
   if (!isChunkJSON) {
+    // no json in chunk found -> append chunk to data collection
     that._data += that._chunk;
     console.log('new data: ' + that._data);
+    // check if data collection contains json object
     var isDataJSON = re.test(that._data);
     console.log('is new data json -> ' + isDataJSON);
     if (isDataJSON) {
+      // that._data plus new chunk now has json object
       var result = re.exec(that._data);
       console.log('result');
       console.log(result);
-      that._data = that._data.split(result[0]).join('');
       if (!this._headerWritten) that.writeHeader(result[0]);
       that.writeLine(result[0], '_data');
     }
@@ -64,19 +70,18 @@ MyStream.prototype._transform = function(chunk, encoding, done) {
  * write the header line
  */
 MyStream.prototype.writeHeader = function(header) {
-  header = JSON.parse(header);
   var that = this;
+  header = JSON.parse(header);
   var iterator = function(item, callback) {
     that._header.push(item);
     callback(null);
   };
   async.each(Object.keys(header), iterator, function(err) {
-    var headerLine = that._header.join(',');
+    var headerLine = that._header.join(that.del);
     console.log(that._header);
     that.emit('header', headerLine);
     that.push(headerLine);
     that._headerWritten = true;
-    that._header = null;
   });
 };
 
@@ -87,17 +92,20 @@ MyStream.prototype.writeLine = function(line, bucket) {
   console.log('inside write line');
   console.log(line);
   var that = this;
-  var lineJson = JSON.parse(line);
+  var lineObject = JSON.parse(line);
   var iterator = function(key, callback) {
-    var val = lineJson[key];
+    var val = lineObject[key];
     that._line.push(val);
     callback(null);
   };
-  async.each(Object.keys(lineJson), iterator, function(err) {
-    var lineStr = that._line.join(',');
+  async.each(Object.keys(lineObject), iterator, function(err) {
+    var lineStr = that._line.join(that.del);
     lineStr = os.EOL + lineStr;
     that.emit('line', lineStr);
     that.push(lineStr);
+    // bucket is either '_data' or '_chunk'
+    // '_data' when json string is found in _data
+    // '_chunk' when json string is found in _chunk
     that._data = that[bucket].split(line).join('');
     that._line = [];
   });
--- test/test.js
+++ test/test.js
@@ -2,22 +2,45 @@
 var should = require('should');
 var fs = require('fs');
 var os = require('os');
+var async = require('async');
 var MyStream = require('../index.js');
 
 describe('json2csv-stream', function() {
 
+  // write some platform independent files to fixtures dir
   before(function(done) {
-    var data =
-      'car,price,color' + os.EOL +
-      'Audi,40000,blue' + os.EOL +
-      'BMW,35000,black' + os.EOL +
-      'Mercedes,80000,red' + os.EOL +
-      'Porsche,60000,green';
+    async.parallel([
+      function(callback) {
+        // comma seperated values
+        var csv_data =
+          'car,price,color' + os.EOL +
+            'Audi,40000,blue' + os.EOL +
+            'BMW,35000,black' + os.EOL +
+            'Mercedes,80000,red' + os.EOL +
+            'Porsche,60000,green';
 
-    fs.writeFile('test/fixtures/out.csv', data, function(err) {
+        fs.writeFile('test/fixtures/out.csv', csv_data, function(err) {
+          callback(err);
+        });
+      },
+      function(callback) {
+        // semicolon seperated values
+        var ssv_data =
+          'car;price;color' + os.EOL +
+            'Audi;40000;blue' + os.EOL +
+            'BMW;35000;black' + os.EOL +
+            'Mercedes;80000;red' + os.EOL +
+            'Porsche;60000;green';
+
+        fs.writeFile('test/fixtures/out_ssv.csv', ssv_data, function(err) {
+          callback(err);
+        });
+      }
+    ], function(err, results) {
       if (err) console.log(err);
       done();
     });
+
   });
 
   it('should parse json to csv when json is in chunk', function(done) {
@@ -43,7 +66,7 @@ describe('json2csv-stream', function() {
 
   });
 
-  it('should parse json to csv when chunk doesn\'t contain json ', function(done) {
+  it('should parse json to csv when chunk doesn\'t contain json', function(done) {
 
     var parser = new MyStream();
     var reader = fs.createReadStream('test/fixtures/in.json', {
@@ -58,6 +81,31 @@ describe('json2csv-stream', function() {
         if (err) console.log(err);
         _data = _data.toString();
         fs.readFile('test/fixtures/out.csv', function(err, data) {
+          if (err) console.log(err);
+          data = data.toString();
+          _data.should.eql(data);
+          done();
+        });
+      });
+    });
+
+  });
+
+  it('should support optional custom delimiters', function(done) {
+
+    var parser = new MyStream({
+      del: ';'
+    });
+    var reader = fs.createReadStream('test/fixtures/in.json');
+    var writer = fs.createWriteStream('test/fixtures/_out_ssv.csv');
+
+    reader.pipe(parser).pipe(writer);
+
+    writer.on('close', function() {
+      fs.readFile('test/fixtures/_out_ssv.csv', function(err, _data) {
+        if (err) console.log(err);
+        _data = _data.toString();
+        fs.readFile('test/fixtures/out_ssv.csv', function(err, data) {
           if (err) console.log(err);
           data = data.toString();
           _data.should.eql(data);
