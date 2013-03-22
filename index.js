65e4b10181dd29a07cb8372e21d91b58ff8c0af1
Mirco <mirco.zeiss@gmail.com>
2013-03-22 21:35:18 +0100

    check node version - closes #2

--- index.js
+++ index.js
@@ -2,10 +2,30 @@
 /**
  * Module dependencies
  */
-var Transform = require('readable-stream').Transform;
 var util = require('util');
 var async = require('async');
 var os = require('os');
+
+/**
+ * Check node version
+ * Taken from https://github.com/isaacs/simple-protocol/blob/master/incoming.js
+ */
+var Transform;
+switch (process.version.match(/v([0-9]+\.[0-9]+)\.[0-9]+/)[1]) {
+  case '0.0': case '0.1':
+  case '0.2': case '0.3':
+  case '0.4': case '0.5':
+  case '0.6': case '0.7':
+  throw new Error('your node is too old and crusty for this hotness');
+
+  case '0.8':
+    Transform = require('readable-stream/transform.js');
+    break;
+
+  default:
+    Transform = require('stream').Transform;
+    break;
+}
 
 /**
  * MyStream constructor function.
